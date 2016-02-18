da0fa1d552a171048e20e21d6e1a7c24168b6655
bingjin <545915891@qq.com>
2016-02-18 22:00:13 +0800

    laden

---
+++ laden/bld.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+git clone https://github.com/bingjin/funscripts.git
+cd funscripts/laden
+pip install -r requirements.txt
+
+git clone https://github.com/AlchemyAPI/alchemyapi_python.git
+cd alchemyapi_python
+python alchemyapi.py YOUR_API_KEY # 将YOUR_API_KEY替换成你的Key
+
+cp bld.py alchempyapi_python/bld.py
+python bld.py
